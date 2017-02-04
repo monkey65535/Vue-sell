b67d4bff61e3c71d1a97d8da702bdafda09b98a2
dilidili <466311467@qq.com>
2017-02-04 22:58:41 +0800

    开始实现header组件

--- vueSell-ver1/src/App.vue
+++ vueSell-ver1/src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="app">
-    
+    <app-header></app-header>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/header/Header.vue';
+export default {
+  components: {
+    AppHeader
+  }
+};
 </script>
 
 <style>
