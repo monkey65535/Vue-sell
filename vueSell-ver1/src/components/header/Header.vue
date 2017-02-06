f4f51656ef3a9541f6e03accab18191a3032f19d
dilidili <466311467@qq.com>
2017-02-06 22:36:35 +0800

    header-end

--- vueSell-ver1/src/components/header/Header.vue
+++ vueSell-ver1/src/components/header/Header.vue
@@ -37,12 +37,36 @@
       <img :src="Seller.avatar" alt="" width="100%" height="100%">
     </div>
     <!--优惠信息遮罩层-->
-    <div class="detail" v-show="detailShow">
+    <div class="detail" v-show="detailShow" transition="fade">
       <div class="detail-warpper clearfix">
         <div class="detail-main">
           <h1 class="name">{{Seller.name}}</h1>
+          <!--star组件-->
           <div class="star-wapper">
             <v-star :size="48" :score="Seller.score"></v-star>
+          </div>
+          <!--优惠信息title-->
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">优惠信息</div>
+            <div class="line"></div>
+          </div>
+          <!--优惠信息-->
+          <ul v-if="Seller.supports" class="supports">
+              <li class="supports-item" v-for="item in Seller.supports">
+                  <span class="icon" :class="classMap[Seller.supports[$index].type]"></span>
+                  <span class="text">{{Seller.supports[$index].description}}</span>
+              </li>
+          </ul>
+          <!--商家公告title-->
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">商家公告</div>
+            <div class="line"></div>
+          </div>
+          <!--商家公告-->
+          <div class="bulletin">
+              <p class="content">{{Seller.bulletin}}</p>
           </div>
         </div>
       </div>
