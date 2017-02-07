1003aadbf68959830c3123282baaa0487f76e3d0
dilidili <466311467@qq.com>
2017-02-07 10:15:51 +0800

    star

--- vueSell-ver1/src/components/star/Star.vue
+++ vueSell-ver1/src/components/star/Star.vue
@@ -1,45 +1,51 @@
 <template>
-  <div class="star" :class="starType">
-    <span v-for="itemClass in itemClasses" :class="itemClass" class="star-item" track-by="$index"></span>
-  </div>
+    <!--外部传递星星尺寸-->
+    <div class="star" :class="starType">
+        <span v-for="itemClass in itemClasses" :class="itemClass" class="star-item star-icon" track-by="$index"></span>
+    </div>
 </template>
 <script>
-    const LENGTH = 5;
-    const CLS_ON = 'on';
-    const CLS_HALF = 'half';
-    const CLS_OFF = 'off';
-    export default {
-        props: {
-            size: {
-                type: Number
-            },
-            score: {
-                type: Number
-            }
+const LENGTH = 5;
+const CLS_ON = 'on';
+const CLS_HALF = 'half';
+const CLS_OFF = 'off';
+export default {
+    data () {
+        return {
+
+        };
+    },
+    props: {
+        size: {
+            type: Number
         },
-        computed: {
-            starType() {
-                return 'star-' + this.size;
-            },
-            itemClasses() {
-                let result = [];
-                let score = Math.floor(this.score * 2) / 2;
-                let hasDec = score % 1 !== 0;
-                let integer = Math.floor(score);
-                for (let i = 0; i < integer; i++) {
-                    result.push(CLS_ON);
-                }
-                if (hasDec) {
-                    result.push(CLS_HALF);
-                }
-                while (result.length < LENGTH) {
-                    result.push(CLS_OFF);
-                }
-                return result;
-            }
+        score: {
+            type: Number
         }
-    };
+    },
+    computed: {
+        starType() {
+            return 'star-' + this.size;
+        },
+        itemClasses() {
+            let result = [];
+            let score = Math.floor(this.score * 2) / 2;
+            let hasDdecimal = score % 1 !== 0;
+            let integer = Math.floor(score);
+            for (let i = 0; i < integer; i++) {
+                result.push(CLS_ON);
+            }
+            if (hasDdecimal) {
+                result.push(CLS_HALF);
+            }
+            while (result.length < LENGTH) {
+                result.push(CLS_OFF);
+            }
+            return result;
+        }
+    }
+};
 </script>
 <style lang="scss" rel="stylesheet/scss">
-  @import "./scss/star.scss";
+   @import "./scss/star.scss";
 </style>
