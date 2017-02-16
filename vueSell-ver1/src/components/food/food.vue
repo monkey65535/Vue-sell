edf8a4af6f4706f07350d72887a1cb48e9ee9b75
dilidili <466311467@qq.com>
2017-02-16 22:58:43 +0800

    food-over

--- vueSell-ver1/src/components/food/food.vue
+++ vueSell-ver1/src/components/food/food.vue
@@ -1,27 +1,33 @@
 <template>
-    <div class="food" v-show="showFlag" transition="move">
-        <div class="food-content">
-            <div class="image-header">
-                <img :src="food.image">
-                <div class="back" @click="hide">
-                    <i class="icon-arrow_lift"></i>
-                </div>
-            </div>
-            <div class="content">
-                <h1 class="title">{{food.name}}</h1>
-                <div class="detail">
-                    <span class="sell-count">月售{{food.sellCount}}份</span>
-                    <span class="rating">好评率{{food.rating}}</span>
-                </div>
-                <div class="price">
-                    <span class="now">￥{{food.price}}</span>
-                    <span class="old" v-if="food.oldPrice">{{food.oldPrice}}</span>
-                </div>
-            </div>
+  <div class="food" v-show="showFlag" transition="move" v-el:food>
+    <div class="food-content">
+      <div class="image-header">
+        <img :src="food.image">
+        <div class="back" @click="hide">
+          <i class="icon-arrow_lift"></i>
         </div>
+      </div>
+      <div class="content">
+        <h1 class="title">{{food.name}}</h1>
+        <div class="detail">
+          <span class="sell-count">月售{{food.sellCount}}份</span>
+          <span class="rating">好评率{{food.rating}}%</span>
+        </div>
+        <div class="price">
+          <span class="now">￥{{food.price}}</span>
+          <span class="old" v-if="food.oldPrice">￥{{food.oldPrice}}</span>
+        </div>
+      </div>
+      <div class="cartcontrol-wrapper" v-show="food.count && food.count>0">
+        <cartcontrol :food="food"></cartcontrol>
+      </div>
+      <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0" transition="fade">加入购物车</div>
     </div>
 </template>
 <script>
+    import Vue from 'vue';
+    import BScroll from 'better-scroll';
+    import cartcontrol from '../cartcontrol/cartcontrol.vue';
     export default {
         props: {
             food: {
@@ -36,13 +42,34 @@
         methods: {
             show() {
                 this.showFlag = true;
+                console.log(this.scroll);
+                this.$nextTick(() => {
+                    if (!this.scroll) {
+                        this.scroll = new BScroll(this.$els.food, {
+                            click: true
+                        });
+                    } else {
+                        this.scroll.refresh();
+                    }
+                });
             },
             hide() {
                 this.showFlag = false;
+            },
+            addFirst(event) {
+                if (!event._constructed) {
+                    return;
+                }
+                this.$dispatch('cart.add', event.target);
+                Vue.set(this.food, 'count', 1);
             }
+        },
+        components: {
+            cartcontrol
         }
     };
 </script>
 <style lang="scss" rel="stylesheet/scss">
   @import "./scss/food.scss";
+
 </style>
